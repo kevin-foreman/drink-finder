071a658f7a58cc82b987e5b227c012ec5658f983
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-03-02 10:56:02 -0600

    Finalized seed files, ready for development with Docker

--- drinkSeed.sql
+++ drinkSeed.sql
@@ -6,12 +6,14 @@ INSERT INTO drinks (name, type, liquor_id) VALUES ("Sailor's Yule", 'Nog', 8);
 INSERT INTO drinks (name, type, liquor_id) VALUES ("Cup O' Jerry", 'Coffee', 8);
 INSERT INTO drinks (name, type, liquor_id) VALUES ('Sevilla', 'Spritz', 2);
 INSERT INTO drinks (name, type, liquor_id) VALUES ('The Number 10', 'Martini', 2);
-INSERT INTO drinks (name, type, liquor_id) VALUES ('', 'Cocktail', 2);
-INSERT INTO drinks (name, type, liquor_id) VALUES ('Vodka Tonic', 'Cocktail', 3);
-INSERT INTO drinks (name, type, liquor_id) VALUES ('Vodka Tonic', 'Cocktail', 3);
-INSERT INTO drinks (name, type, liquor_id) VALUES ('Vodka Tonic', 'Cocktail', 3);
-INSERT INTO drinks (name, type, liquor_id) VALUES ('Vodka Tonic', 'Cocktail', 4);
-INSERT INTO drinks (name, type, liquor_id) VALUES ('Vodka Tonic', 'Cocktail', 4);
-INSERT INTO drinks (name, type, liquor_id) VALUES ('Vodka Tonic', 'Cocktail', 4);
+INSERT INTO drinks (name, type, liquor_id) VALUES ('London Dry', 'Negroni', 2);
+INSERT INTO drinks (name, type, liquor_id) VALUES ('Neat', 'Whisky', 3);
+INSERT INTO drinks (name, type, liquor_id) VALUES ('Summer Nights', 'Cocktail', 4);
+INSERT INTO drinks (name, type, liquor_id) VALUES ('Blackberry Smash', 'Sipper', 4);
+INSERT INTO drinks (name, type, liquor_id) VALUES ('Dirty Shirley', 'Old-school', 4);
+INSERT INTO drinks (name, type, liquor_id) VALUES ('Pineapple Rum', 'Cocktail', 5);
+INSERT INTO drinks (name, type, liquor_id) VALUES ('Island Rum', 'Punch', 5);
+INSERT INTO drinks (name, type, liquor_id) VALUES ("Captain's Passion", 'Punch', 5);
+
 
 
