e93c1aabc9673a36d7141c9eb61c604bea6db479
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-03-02 10:58:52 -0600

    Fixed an error in drinkSeed

--- liquorSeed.sql
+++ liquorSeed.sql
@@ -6,4 +6,4 @@ INSERT INTO liquor (name, proof) VALUES ('Captain Morgan', 100);
 INSERT INTO liquor (name, proof) VALUES ('Wild Turkey Rare Breed', 112);
 INSERT INTO liquor (name, proof) VALUES ('Everclear', 190);
 INSERT INTO liquor (name, proof) VALUES ('Sailor Jerry', 92);
-INSERT INTO liquor (name, proof) VALUES ('Tanqueray', 94);
+INSERT INTO liquor (name, proof) VALUES ('Parrot Bay', 42);
