ab415f9e54e39df460dd755a0bcdb64617947225
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-03-02 17:16:02 -0600

    Added some routes for patch and delete

--- restfulDrinkServer.js
+++ restfulDrinkServer.js
@@ -23,7 +23,7 @@ app.get('/api/drinks', (req, res, next) => {
         }
         const rows = result.rows;
         console.log(rows);
-        res.send(rows); 
+        res.send(rows);
     });
 });
 
@@ -34,7 +34,7 @@ app.get('/api/liquor', (req, res, next) => {
         }
         const rows = result.rows;
         console.log(rows);
-        res.send(rows); 
+        res.send(rows);
     });
 });
 
@@ -83,10 +83,28 @@ app.post('/api/liquor/', (req, res, next) => {
     };
 });
 
+app.post('/api/liquor/', (req, res, next) => {
+    const { name, proof } = req.body;
+
+    if (!name || !proof) {
+        return res.status(400).send('Error: missing values')
+    } else {
+
+        pool.query('INSERT INTO liquor (name, proof) VALUES ($1, $2) RETURNING *;', [name, proof], (err, result) => {
+            if (err) {
+                return next(err);
+            };
+            let liquorInfo = result.rows[0];
+            console.log('Added: ' + liquorInfo);
+            res.status(200).send(liquorInfo);
+        });
+    };
+});
+
 app.post('/api/drinks/', (req, res, next) => {
     const { name, type, liquor_id } = req.body;
 
-    if (!name || !type || !liquor_id ) {
+    if (!name || !type || !liquor_id) {
         return res.status(400).send('Error: missing values')
     } else {
 
@@ -99,6 +117,89 @@ app.post('/api/drinks/', (req, res, next) => {
             res.status(200).send(drinkInfo);
         });
     };
+});
+
+app.patch('/api/drinks/:id', (req, res, next) => {
+    // parse id from URL
+    const id = Number.parseInt(req.params.id);
+    // get data from request body
+    const liquor_id = Number.parseInt(req.body.proof);
+    const { name, type } = req.body;
+    // if id input is ok, make DB call to get existing values
+    if (!Number.isInteger(liquor_id)) {
+        res.status(400).send("No drink found with that ID");
+    }
+    console.log("drinkID: ", id);
+    // get current values of the pet with that id from our DB
+    pool.query('SELECT * FROM drinks WHERE id = $1', [id], (err, result) => {
+        if (err) {
+            return next(err);
+        }
+        console.log("request body name, type, liquor_id: ", name, type, liquor_id);
+        const drink = result.rows[0];
+        console.log("Single drink ID from DB", id, "values:", drink);
+        if (!drink) {
+            return res.status(404).send("No drink found with that ID");
+        } else {
+            const updatedName = name || drink.name;
+            const updatedType = type || drink.type;
+            const updatedLiquorId = liquor_id || drink.liquor_id;
+
+            pool.query('UPDATE drinks SET name=$1, type=$2, liquor_id=$3 WHERE id = $4 RETURNING *',
+                [updatedName, updatedType, updatedLiquorId, id], (err, data) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    const updatedDrink = data.rows[0];
+                    console.log("updated row:", updatedDrink);
+                    return res.send(updatedDrink);
+                });
+        };
+    });
+});
+
+app.delete("/api/liquor/:id", (req, res, next) => {
+    const id = Number.parseInt(req.params.id);
+    if (!Number.isInteger(id)) {
+        return res.status(400).send("No liquor found with that ID");
+    }
+
+    pool.query('DELETE FROM liquor WHERE id = $1 RETURNING *', [id], (err, data) => {
+        if (err) {
+            return next(err);
+        }
+
+        const deletedLiquor = data.rows[0];
+        console.log(deletedLiquor);
+        if (deletedLiquor) {
+            // respond with deleted row
+            res.send(deletedLiquor);
+        } else {
+            res.status(404).send("No liquor found with that ID");
+        }
+    });
+});
+
+app.delete("/api/drinks/:id", (req, res, next) => {
+    const id = Number.parseInt(req.params.id);
+    if (!Number.isInteger(id)) {
+        return res.status(400).send("No drink found with that ID");
+    }
+
+    pool.query('DELETE FROM drinks WHERE id = $1 RETURNING *', [id], (err, data) => {
+        if (err) {
+            return next(err);
+        }
+
+        const deletedDrink = data.rows[0];
+        console.log(deletedDrink);
+        if (deletedDrink) {
+            // respond with deleted row
+            res.send(deletedDrink);
+        } else {
+            res.status(404).send("No drink found with that ID");
+        }
+    });
 });
 
 const port = process.env.port || 8000;
