8c779a0ca33786a10c65c32a554e236022efa864
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-03-06 16:24:41 -0600

    Removed bug from index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 8000;
 
-server.use((req, res) => {
-    res.send('Hello Drinks!');
-});
+// server.use((req, res) => {
+//     res.send('Hello Drinks!');
+// });
 
 $('#drinkPick').on('click', function () {
     let drinkSearch = $('#userSearch').val();
@@ -29,9 +29,9 @@ $('#drinkPick').on('click', function () {
         if (image === null) {
             image = 'https://uxwing.com/wp-content/themes/uxwing/download/web-app-development/image-not-found-icon.png';
         } else {
-            image = dataObj.data.results[0].thumbnail.path + '/standard_xlarge.jpg';
+            // image = dataObj.data.results[0].thumbnail.path + '/standard_xlarge.jpg';
         };
-        let description = dataObj.data.results[0].description;
+        let type = dataObj.data.results[0].type;
         let drinks = dataObj.data.results[0].drinks.items; // .join(', ');  
         let sortedDrinks = [];
         for (let i = 0; i < drinks.length; i++) {
@@ -43,12 +43,12 @@ $('#drinkPick').on('click', function () {
         $(`#drinkResult`).append(`<span class='result-card' id='span'></span>`);
         $(`#span`).append(`<h2 class='card-title' id='header'>${header}</h3>`);
         $(`#span`).append(`<img src='${image}' class='card' id='image'>`);
-        $(`#span`).append(`<h3 class='description' id='subheader'>${description}</h2>`);
-        $(`#span`).append(`<h4 class='comicbooks' id='comics'>Comic books that ${header} has been in</h4>`);
+        $(`#span`).append(`<h3 class='type' id='subheader'>${type}</h2>`);
+        // $(`#span`).append(`<h4 class='comicbooks' id='comics'>Comic books that ${header} has been in</h4>`);
         $(`#span`).append(`<h4 class='comicbooks' id='comics'>${sortedDrinks}</h4>`);
     });
 });
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-  });
+});
