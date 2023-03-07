6ee5195e4f96becfc7fdb842d9b0bf3841add613
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-03-07 11:48:52 -0600

    Added support for access to xmlhttprequest

--- restfulDrinkServer.js
+++ restfulDrinkServer.js
@@ -12,7 +12,17 @@ const port = process.env.port || 8000;
 const dbConn = require('./dbConn');
 const pool = dbConn.getPool();
 
-app.use(express.json()); 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS'); // include OPTIONS method
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200); // respond OK to OPTIONS requests
+    }
+    next();
+});
+
+app.use(express.json());
 
 app.get('/api/drinks', (req, res, next) => {
     pool.query("Select * FROM drinks", (err, result) => {
