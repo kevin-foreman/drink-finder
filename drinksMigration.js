93431b092850f2ae94e41a11ac3ec6393de91d35
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-03-09 09:17:02 -0600

    Updated migration files for CommonJS syntax

--- drinksMigration.js
+++ drinksMigration.js
@@ -1,4 +1,6 @@
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
+import pkg from 'pg';
+const { Pool } = pkg;
 const DATABASE_URL = process.env.DATABASE_URL;
 import { getPool } from './dbConn.js';
 const pool = getPool();
